dede9bb98c37b9e0ea8e0ca1418c298887570e08
Ryo Motoki <lalalararirureryo@gmail.com>
2018-03-01 04:26:31 +0900

    a

--- WebSampleApp/ViewController.swift
+++ WebSampleApp/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate {
+    
+    @IBOutlet weak var titleBar: UINavigationBar!
+    @IBOutlet var webView: UIWebView!
+    var webUrl: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.titleBar.topItem?.title = "WebPage"
+        
+        webView.delegate = self
+        webView.scalesPageToFit = true
+        
+        let url: URL = URL(string: "https://www.youtube.com/watch?v=1xp4__OVOpY")!
+        let request: URLRequest = URLRequest(url: url)
+        
+        webView.loadRequest(request)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        print("indicator on")
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        print("indicator off")
+    }
+    
+    @IBAction func goBack() {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func goFoward() {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
 }
 
